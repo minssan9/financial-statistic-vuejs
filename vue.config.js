88f1b0e14278ace0fc61367f71b0851c486b3a0e
민상훈 <minssan9@gmail.com>
2020-12-07 15:41:05 +0900

    vue-cli 3 webpack
    npm i -g @vue/cli
    npm install webpack@^4.0.0 --save-dev

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+// module.exports = {
+//     devServer: {
+//       proxy: {
+//         "/api": {
+//           target: "localhost:54000",
+//           ws: true,
+//           changeOrigin: true
+//         }
+//       }
+//     }
+//   }
