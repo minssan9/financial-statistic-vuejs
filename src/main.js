3994efb1a9e8dc5f4a36694d96d5c6659fb54d4b
Min <minssan9@gmail.com>
2021-02-03 17:49:21 +0900

    config.js changed
    to vue-cli-service

--- src/main.js
+++ src/main.js
@@ -2,12 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import router from './router'
-import store from './store/index.js'
+// import store from './store/index.js'
+
+import { IconsPlugin } from 'bootstrap-vue'
+
+
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 new Vue({
     el: '#app',
     router,
     axios,
-    store,
+    // store,
     render: h => h(App)
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import store from '../store'
+// import store from '../store'
 
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const requireAuth = (to, from, next) => {
-    !!store.state.access_token ? next() : next(`/login?returnPath=${encodeURIComponent(from.path)}`)
-}
-
-const requireManager = (to, from, next) => {
-
-    // 액세스토큰이 있으면
-    if (!!store.state.access_token) {
-        // 권한이 ADMIN 이면
-        if (!!store.state.account.roles && !!(store.state.account.roles.includes('ADMIN') ))
-            return next()
-        else {
-            alert('권한이 없습니다.')
-            return next('/')
-        }
-    } else
-        return next(`/login?returnPath=${encodeURIComponent(from.path)}`)
-
-
-}
+// const requireAuth = (to, from, next) => {
+//     !!store.state.access_token ? next() : next(`/login?returnPath=${encodeURIComponent(from.path)}`)
+// }
+//
+// const requireManager = (to, from, next) => {
+//
+//     // 액세스토큰이 있으면
+//     if (!!store.state.access_token) {
+//         // 권한이 ADMIN 이면
+//         if (!!store.state.account.roles && !!(store.state.account.roles.includes('ADMIN') ))
+//             return next()
+//         else {
+//             alert('권한이 없습니다.')
+//             return next('/')
+//         }
+//     } else
+//         return next(`/login?returnPath=${encodeURIComponent(from.path)}`)
+// }
 
 const router = new VueRouter({
     mode: 'history',
@@ -40,14 +38,14 @@ const router = new VueRouter({
             path: '/',
             component: Home
         },
-        {
-            path: '/join',
-            component: Join
-        },
-        {
-            path: '/login',
-            component: Login
-        },
+        // {
+        //     path: '/join',
+        //     component: Join
+        // },
+        // {
+        //     path: '/login',
+        //     component: Login
+        // },
     ]
 })
 
